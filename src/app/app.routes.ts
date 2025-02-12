a64065b80faf1d5b293fe4acb768a95e74472e8a
dmitryprogrammer <dpischalka@gmail.com>
2025-02-13 00:20:48 +0300

    feat(base): prepared base for movies component store practice.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'movies',
+  },
+  {
+    path: 'movies',
+    loadComponent: () =>
+      import('./pages/movies/components/movies/movies.component').then((m) => m.MoviesComponent),
+  },
+];
