5f7d4cd19f94003311acc2f5d89fad99d693b61b
dmitryprogrammer <dpischalka@gmail.com>
2025-02-16 17:01:41 +0300

    feat(base): added and integrated add movie component to main component.

--- src/app/pages/movies/components/movies/movies.component.ts
+++ src/app/pages/movies/components/movies/movies.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Movie, MoviesStore } from '../../store/movies.store';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { AddMovieComponent } from '../add-movie/add-movie.component';
 
 @Component({
   selector: 'app-movies',
-  imports: [CommonModule],
+  imports: [CommonModule, AddMovieComponent],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
