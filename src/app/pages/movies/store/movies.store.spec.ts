787552e984079b4fe0c03e1fb4268fb68de8ffc3
dmitryprogrammer <dpischalka@gmail.com>
2025-03-02 00:09:09 +0300

    feat(base): added specs for "movies.store".

---
+++ src/app/pages/movies/store/movies.store.spec.ts
@@ -0,0 +1,42 @@
+import { TestBed } from '@angular/core/testing';
+import { MoviesStore } from './movies-store';
+import { Movie } from './movie';
+
+describe('MoviesStore', () => {
+  let moviesStore: MoviesStore;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [MoviesStore],
+    });
+
+    moviesStore = TestBed.inject(MoviesStore);
+  });
+
+  it('should create', () => {
+    expect(moviesStore).toBeTruthy();
+  });
+
+  it('should initialize movies', () => {
+    expect(moviesStore.movies$).toBeTruthy();
+  });
+
+  it('should initialize latestMovieId', () => {
+    expect(moviesStore.latestMovieId$).toBeTruthy();
+  });
+
+  it('should add movie', () => {
+    const movie: Movie = {
+      id: 1,
+      title: 'Test Movie',
+      year: 2025,
+      rating: 5,
+    };
+
+    moviesStore.addMovie(movie);
+
+    moviesStore.movies$.subscribe((movies) => {
+      expect(movies).toContain(movie);
+    });
+  });
+});
